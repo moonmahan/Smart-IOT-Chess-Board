ff3574421ec87e171402cd885919d403c787a285
moonmahan <42904344+moonmahan@users.noreply.github.com>
2018-09-03 11:24:22 +0430

    Add files via upload

---
+++ magnetSensor.ino
@@ -0,0 +1,20 @@
+int sens=5;
+int led=9;
+int out;
+void setup() {
+  pinMode(led,OUTPUT);
+  pinMode(sens,INPUT);
+}
+
+void loop() {
+
+  out=digitalRead(sens);
+  if(out==LOW){
+    digitalWrite(led,HIGH);
+  }
+   if(out==HIGH){
+    digitalWrite(led,LOW);
+  }
+
+
+}
